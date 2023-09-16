9c6b31bfd1d3696170c6deb953375a542a9f78ad
Zilant1337 <grigorenko_p@inbox.ru>
2023-09-16 14:14:48 +0300

    Added Point2D Class

--- src/Point2D.java
+++ src/Point2D.java
@@ -1,2 +1,19 @@
-public class Point2D {
+public class Point2D extends Point {
+    public Point2D(double[] x){
+        super(2,x);
+    }
+    public Point2D() {
+        super(2);
+    }
+    public static Point2D rot(Point2D p, double phi) {
+        return new Point2D(new double[]{ p.x[0] * Math.cos(phi) - p.x[1] * Math.sin(phi), p.x[0] * Math.sin(phi) + p.x[1] * Math.cos(phi)});
+    }
+
+    public Point2D rot(double phi)
+    {
+        Point2D newPoint = rot(this, phi);
+        x = newPoint.x;
+        dim = newPoint.dim;
+        return newPoint;
+    }
 }
